ae34b2ed5ce87af9cd3cad79be6145108d608f79
harshan-sree <144901898+harshan-sree@users.noreply.github.com>
2024-06-14 13:11:05 +0530

    Add files via upload

---
+++ Average_Calculator/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import AverageCalculator from "./Calculator";
+function App() {
+  return (
+    <div className="App">
+    <AverageCalculator />
+</div>
+  );
+}
+
+export default App;
